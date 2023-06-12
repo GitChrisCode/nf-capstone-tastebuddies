65954a121ee2875605e1ff0a308dc68fd713af1e
GitChrisCode <chrha.gw2@gmail.com>
2023-06-12 14:14:16 +0200

    Backend: adding SpringSecurity

---
+++ backend/src/main/java/de/neuefische/backend/service/MongoUserDetailsService.java
@@ -0,0 +1,24 @@
+package de.neuefische.backend.service;
+import de.neuefische.backend.model.UserTasteBuddies;
+import de.neuefische.backend.repository.MongoUserTasteBuddiesRepo;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class MongoUserDetailsService implements UserDetailsService {
+
+    private final MongoUserTasteBuddiesRepo repo;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserTasteBuddies mongoUser = repo.findUserTasteBuddiesByUserName(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User with username:" + username + " not found"));
+        return new User(mongoUser.getUserName(), mongoUser.getUserPassword(), List.of());
+    }
+}
