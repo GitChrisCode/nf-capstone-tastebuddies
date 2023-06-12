166ced1f7bef35697f0a48fb7507199c91eb11ae
GitChrisCode <chrha.gw2@gmail.com>
2023-06-12 12:25:42 +0200

    Backend: added MongoDB support

---
+++ backend/src/main/java/de/neuefische/backend/repository/RecipeRepository.java
@@ -0,0 +1,8 @@
+package de.neuefische.backend.repository;
+import de.neuefische.backend.model.Recipe;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class RecipeRepository {
+}
