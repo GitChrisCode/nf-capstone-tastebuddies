3a0a62fd12657c633129a6cdf03220788fdaaed2
GitChrisCode <chrha.gw2@gmail.com>
2023-06-13 15:53:19 +0200

    Backend: added new test for registration

--- backend/src/main/java/de/neuefische/backend/model/UserTasteBuddies.java
+++ backend/src/main/java/de/neuefische/backend/model/UserTasteBuddies.java
@@ -2,6 +2,7 @@ package de.neuefische.backend.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
--- backend/src/test/java/de/neuefische/backend/controller/UserTasteBuddiesControllerTest.java
+++ backend/src/test/java/de/neuefische/backend/controller/UserTasteBuddiesControllerTest.java
@@ -1,12 +1,18 @@
 package de.neuefische.backend.controller;
 
+import de.neuefische.backend.model.UserTasteBuddies;
+import de.neuefische.backend.service.UserTasteBuddiesDetailsService;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.*;
@@ -32,5 +38,26 @@ class UserTasteBuddiesControllerTest {
                 .andExpect(status().isCreated())
                 .andExpect(MockMvcResultMatchers.jsonPath("$.userName").value(userName));
 
+    }
+
+    @Test
+    public void testRegisterUserTasteBuddiesV2() {
+        // Erstellen Sie eine Mock-Instanz des UserTasteBuddiesDetailsService
+        UserTasteBuddiesDetailsService mockService = Mockito.mock(UserTasteBuddiesDetailsService.class);
+
+        // Definieren Sie das erwartete Verhalten der Mock-Instanz
+        UserTasteBuddies expectedUser = new UserTasteBuddies();
+        expectedUser.setUserName("testUser");
+        expectedUser.setUserPassword("testPassword");
+        Mockito.when(mockService.registerUserTasteBuddies(anyString(), anyString())).thenReturn(expectedUser);
+
+        // Erstellen Sie eine Instanz des Controllers mit dem Mock-Service
+        UserTasteBuddiesController controller = new UserTasteBuddiesController(mockService);
+
+        // Rufen Sie die Methode des Controllers auf, die getestet werden soll
+        UserTasteBuddies resultUser = controller.registerUserTasteBuddies("TestUser", "TestPassword");
+
+        // Überprüfen Sie das Ergebnis
+        assertEquals(expectedUser, resultUser);
     }
 }
