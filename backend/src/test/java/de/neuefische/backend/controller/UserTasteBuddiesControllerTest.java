55293ac0e70e3f640da839ff38b31ec3da72b927
GitChrisCode <chrha.gw2@gmail.com>
2023-06-13 15:12:36 +0200

    Backend: added missing dependency

--- backend/src/test/java/de/neuefische/backend/controller/UserTasteBuddiesControllerTest.java
+++ backend/src/test/java/de/neuefische/backend/controller/UserTasteBuddiesControllerTest.java
@@ -1,6 +1,5 @@
 package de.neuefische.backend.controller;
 
-import de.neuefische.backend.security.SecurityConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
