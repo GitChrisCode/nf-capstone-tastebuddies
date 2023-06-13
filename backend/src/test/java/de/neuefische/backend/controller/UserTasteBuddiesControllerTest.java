c9230c9cc4d7a93949444211b8996ab74ee6cfde
GitChrisCode <chrha.gw2@gmail.com>
2023-06-13 14:28:27 +0200

    Backend: added @DirtiesContext to Test

--- backend/src/test/java/de/neuefische/backend/controller/UserTasteBuddiesControllerTest.java
+++ backend/src/test/java/de/neuefische/backend/controller/UserTasteBuddiesControllerTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -17,6 +18,7 @@ public class UserTasteBuddiesControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+    @DirtiesContext
     @Test
     public void testRegisterUserTasteBuddies() throws Exception {
         //given
