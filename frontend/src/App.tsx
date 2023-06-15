67a655b8a32453fcaf2589875cd32442d790974c
GitChrisCode <chrha.gw2@gmail.com>
2023-06-15 09:46:42 +0200

    Frontend: added a test page component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,6 +6,7 @@ import {Route, Routes} from "react-router-dom";
 import LoginPage from "./components/LoginPage";
 import RecipeSearch from "./components/RecipeSearch";
 import ProtectedRoutes from "./components/Protected Routes";
+import Test from "./components/Test";
 
 function App() {
 
@@ -15,14 +16,17 @@ function App() {
       <div>
           <header>
               <h1>TasteBuddies</h1>
+
           </header>
           <Routes>
+              <Route path="/" element={<LoginPage login={login}/>}/>
               <Route path="/login" element={<LoginPage login={login}/>}/>
               <Route path="/register" element={<RegisterForm/>}/>
+              <Route path="/test" element={<Test/>}/>
               <Route element={<ProtectedRoutes user={user}/>}>
-                <Route path="/recipesearch" element={<RecipeSearch/>}
-                />
+                <Route path="/recipesearch" element={<RecipeSearch/>}/>
               </Route>
+
           </Routes>
       </div>
   );
