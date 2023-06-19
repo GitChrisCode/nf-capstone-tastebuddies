8eb657bde4ea3752c8c4da2d43082168fcfd8f39
GitChrisCode <chrha.gw2@gmail.com>
2023-06-19 17:24:50 +0200

    Frontend extended RecipeSearch component

--- frontend/src/components/RecipeSearch.tsx
+++ frontend/src/components/RecipeSearch.tsx
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, {FormEvent, useState} from 'react';
 import LogoutButton from "./LogoutButton";
+import axios from "axios";
+
+type Recipes = {
+    id: number,
+    title: string,
+    image: string,
+    imageType: string
+
+}
 
 function RecipeSearch() {
+    const [searchQuery, setSearchQuery] = useState("");
+    const [recipesSearchResult, setRecipesSearchResult] = useState<Recipes[]>([]);
+    function searchSubmit(e: FormEvent<HTMLFormElement>) {
+        e.preventDefault();
+        axios.get("/tb/user/recipes/complexSearch?query=" + searchQuery)
+            .then(response => response.data)
+            .catch(console.error)
+            .then((data) => setRecipesSearchResult(data.results))
+    }
 
         return (
             <div>
-             <h1>Work in Progress</h1>
+             <h1>Search Recipe:</h1>
+                <form onSubmit={searchSubmit}>
+                    <p>Enter Ingredients:</p>
+                    <input type="text" onChange={(e)=> setSearchQuery(e.target.value)}/>
+                </form>
                 <LogoutButton/>
             </div>
         );
