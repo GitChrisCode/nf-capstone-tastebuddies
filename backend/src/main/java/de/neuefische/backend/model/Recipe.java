625bf40c36b02e08178365c410b0f5c7034d7234
GitChrisCode <chrha.gw2@gmail.com>
2023-06-19 21:38:30 +0200

    removed old import from recipe model

--- backend/src/main/java/de/neuefische/backend/model/Recipe.java
+++ backend/src/main/java/de/neuefische/backend/model/Recipe.java
@@ -2,7 +2,7 @@ package de.neuefische.backend.model;
 
 import lombok.*;
 import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
