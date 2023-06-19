81d2cb0de4e87347537a4612b8700b861eb80664
GitChrisCode <chrha.gw2@gmail.com>
2023-06-19 16:46:56 +0200

    Backend: extended Recipe models

---
+++ backend/src/main/java/de/neuefische/backend/model/RecipeCollection.java
@@ -0,0 +1,15 @@
+package de.neuefische.backend.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class RecipeCollection {
+    private List<Recipe> recipes;
+    private RecipeInfo recipeInfo;
+}
