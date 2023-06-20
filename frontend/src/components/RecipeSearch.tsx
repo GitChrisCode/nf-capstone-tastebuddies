febf931f7fbced488a87d3370106e768b4fc998d
GitChrisCode <chrha.gw2@gmail.com>
2023-06-20 08:38:00 +0200

    Frontend: modified search result presentation

--- frontend/src/components/RecipeSearch.tsx
+++ frontend/src/components/RecipeSearch.tsx
@@ -1,50 +1,52 @@
-import React, {FormEvent, useState} from 'react';
-import LogoutButton from "./LogoutButton";
-import axios from "axios";
+import React, { FormEvent, useState } from 'react';
+import LogoutButton from './LogoutButton';
+import axios from 'axios';
+import '../css/RecipeSearch.css'
 
 type Recipes = {
-    id: number,
-    title: string,
-    image: string,
-    imageType: string
-}
+    id: number;
+    title: string;
+    image: string;
+    imageType: string;
+};
 
 function RecipeSearch() {
-    const [searchQuery, setSearchQuery] = useState("");
+    const [searchQuery, setSearchQuery] = useState('');
     const [recipesSearchResult, setRecipesSearchResult] = useState<Recipes[]>([]);
+
     function searchSubmit(e: FormEvent<HTMLFormElement>) {
         e.preventDefault();
-        axios.get("/tb/user/recipesearch?query=" + searchQuery)
+        axios
+            .get('/tb/user/recipesearch?query=' + searchQuery)
             .then(response => response.data)
             .catch(console.error)
-            .then((data) => setRecipesSearchResult(data.results))
+            .then(data => setRecipesSearchResult(data.results));
     }
 
-        return (
-            <div>
-             <h1>Search Recipe:</h1>
-                <form onSubmit={searchSubmit}>
-                    <p>Enter Ingredients:</p>
-                    <input type="text" onChange={(e)=> setSearchQuery(e.target.value)}/>
-                    <button> Search</button>
-                </form>
-                <LogoutButton/>
-                <h2>Search Results:</h2>
-                {recipesSearchResult.length > 0 ? (
-                    <ul>
-                        {recipesSearchResult.map(recipe => (
-                            <li key={recipe.id}>
-                                <img src={recipe.image} alt={recipe.title} />
-                                <h3>{recipe.title}</h3>
-                            </li>
-                        ))}
-                    </ul>
-                ) : (
-                    <p>No recipes found.</p>
-                )}
-            </div>
-        );
-
+    return (
+        <div>
+            <h1>Search Recipe:</h1>
+            <form onSubmit={searchSubmit}>
+                <p>Enter Ingredients:</p>
+                <input type="text" onChange={e => setSearchQuery(e.target.value)} />
+                <button>Search</button>
+            </form>
+            <LogoutButton />
+            <h2>Search Results:</h2>
+            {recipesSearchResult.length > 0 ? (
+                <div className="grid-container">
+                    {recipesSearchResult.map(recipe => (
+                        <div key={recipe.id} className="grid-item">
+                            <img src={recipe.image} alt={recipe.title} />
+                            <h3>{recipe.title}</h3>
+                        </div>
+                    ))}
+                </div>
+            ) : (
+                <p>No recipes found.</p>
+            )}
+        </div>
+    );
 }
 
 export default RecipeSearch;
