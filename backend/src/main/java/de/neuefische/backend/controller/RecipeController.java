27ec6a34fa98114c65c4d3d372ac595388ac6c42
GitChrisCode <chrha.gw2@gmail.com>
2023-06-22 12:28:51 +0200

    Backend: add new endpoint for feature recipe details

--- backend/src/main/java/de/neuefische/backend/controller/RecipeController.java
+++ backend/src/main/java/de/neuefische/backend/controller/RecipeController.java
@@ -1,18 +1,36 @@
 package de.neuefische.backend.controller;
 
 import de.neuefische.backend.model.RecipeCollection;
+import de.neuefische.backend.model.RecipeInformation;
 import de.neuefische.backend.service.RecipeService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/tb/user")
 @RequiredArgsConstructor
 public class RecipeController {
+
     private final RecipeService service;
 
     @GetMapping("/recipesearch")
     public RecipeCollection getRecipes(@RequestParam("query") String searchQuery) {
         return service.getRecipes(searchQuery);
+    }
+
+    @GetMapping("/recipe/{id}")
+    public ResponseEntity<RecipeInformation> getRecipeDetail(@PathVariable("id") Integer id) {
+        try {
+            RecipeInformation recipeDetail = service.getRecipeDetail(id);
+            if (recipeDetail != null) {
+                return ResponseEntity.ok(recipeDetail);
+            } else {
+                return ResponseEntity.notFound().build();
+            }
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
     }
 }
