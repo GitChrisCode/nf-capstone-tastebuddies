8cb587844b4298705e95ff20164aa9d5c762c251
GitChrisCode <chrha.gw2@gmail.com>
2023-06-27 17:07:19 +0200

    setting up personal preferences and dislikes

---
+++ frontend/src/components/Ingredients.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+function ListOfIngredients() {
+
+
+
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default ListOfIngredients;
--- frontend/src/components/Test.tsx
+++ frontend/src/components/Test.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import CSVReader from "./Ingredients";
 
 function Test() {
     return (
         <div>
             <h1>Gogo Gadget!</h1>
+            <CSVReader/>
         </div>
     );
 }
